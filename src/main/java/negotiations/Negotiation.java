c1fafdccc17f0de95db3f9f987cab906a4b3188a
patrickmcdermott <patrickmcdermott@patricks-mbp.fios-router.home>
2020-08-04 14:07:56 -0400

    added a negotiation class

---
+++ src/main/java/negotiations/Negotiation.java
@@ -0,0 +1,29 @@
+package negotiations;
+
+import javax.management.relation.Role;
+
+public class Negotiation {
+  
+  /**
+   * Holds negotiation information.
+   */
+  
+  private Role userRole;
+  
+  public Negotiation(Role userRole) {
+    this.userRole = userRole;
+  }
+  
+  public Role getUserRole() {
+    return userRole;
+  }
+
+  public void setUserRole(Role userRole) {
+    this.userRole = userRole;
+  }
+
+  
+  
+  
+
+}
