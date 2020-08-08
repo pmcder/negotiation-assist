dfeefa9c38546772fac6c93df53710c117bb9766
patrickmcdermott <patrickmcdermott@patricks-mbp.fios-router.home>
2020-08-07 20:03:09 -0400

    added memento test

--- src/test/java/memento/RoleMementoTest.java
+++ src/test/java/memento/RoleMementoTest.java
@@ -10,7 +10,7 @@ import roles.Role;
 public class RoleMementoTest {
 
   @Test
-  public void test() {
+  public void testUndo() {
     Role testBuyer = new Buyer();
     CareTaker careTaker = new CareTaker();
     testBuyer.move(20000);
@@ -20,6 +20,18 @@ public class RoleMementoTest {
     testBuyer.move(16200);
     testBuyer.undo(careTaker.getLastMemento());
     assertEquals(i, testBuyer.getCurrent());
+  }
+  
+  @Test
+  public void testNotUndo() {
+    Role testBuyer = new Buyer();
+    CareTaker careTaker = new CareTaker();
+    testBuyer.move(20000);
+    testBuyer.move(17000);
+    int i = testBuyer.getCurrent();
+    careTaker.addMemento(testBuyer.setMemento());
+    testBuyer.move(16200);
+    assertNotEquals(i, testBuyer.getCurrent());
   }
 
 }
